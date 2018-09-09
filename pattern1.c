ea7e8ac64347c2fc2894c54b66785b5abfd13151
Shrimant More <smshrimant@gmail.com>
2018-09-09 11:52:03 +0530

    New file added

---
+++ pattern1.c
@@ -0,0 +1,22 @@
+/*
+
+*
+**
+***
+
+*/
+
+#include<stdio.h>
+int main()													
+{
+	for(int j=0;j<3;j++)
+	{
+		for(int i=0;i<=j;i++)
+		{
+		 printf("*");
+		}
+	printf("\n");
+	}	
+return 0;
+
+}
