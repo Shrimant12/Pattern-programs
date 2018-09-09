90c3c478cd268ed82ad89faceee6e941a45aa54e
Shrimant More <smshrimant@gmail.com>
2018-09-09 12:04:35 +0530

    New file added

---
+++ pattern4.c
@@ -0,0 +1,25 @@
+/*
+
+1
+23
+456
+
+*/
+
+#include<stdio.h>
+int main()
+{
+	int num = 1;
+	for(int i=1;i<4;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			printf("%d",num++);
+		
+		}
+		printf("\n");
+	}
+
+
+return 0;
+}
